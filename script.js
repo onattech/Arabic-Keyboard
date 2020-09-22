be4edd747ab07011a315b6bc419794af7582eb10
Loku <onatim@protonmail.com>
2020-09-22 21:50:37 +0300

    Fixed pointer-events=none

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 function handleChange(){
-    console.log('changed')
-    document.getElementById('keyboard').scrollTop = 10000
-    window.scrollTo(0,document.body.scrollHeight);
+    // console.log('changed')
+    // window.scrollTo(0,document.body.scrollHeight);
 }
 
+function add(c) {
+    console.log(document.getElementById("text").textContent)
+    document.getElementById("text").innerHTML += c
+}
